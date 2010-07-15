19f277a931513cef0beeb71020bc0ffeb7ab2198
schoenborn <schoenborn@a41a61bc-0f84-421d-9ab3-fe1244b22163>
2010-07-15 12:05:53 +0000

    LegacyCommandCache no longer caches itself

--- src/main/java/de/cosmocode/palava/ipc/legacy/LegacyCommandCache.java
+++ src/main/java/de/cosmocode/palava/ipc/legacy/LegacyCommandCache.java
@@ -17,16 +17,12 @@
 package de.cosmocode.palava.ipc.legacy;
 
 import java.util.Set;
-import java.util.concurrent.ConcurrentMap;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Preconditions;
-import com.google.common.collect.MapMaker;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 
+import de.cosmocode.commons.reflect.Classes;
 import de.cosmocode.palava.bridge.command.Alias;
 
 /**
@@ -36,10 +32,6 @@ import de.cosmocode.palava.bridge.command.Alias;
  * @author Willi Schoenborn
  */
 final class LegacyCommandCache implements CommandCache {
-
-    private static final Logger LOG = LoggerFactory.getLogger(LegacyCommandCache.class);
-
-    private final ConcurrentMap<String, Class<?>> cache = new MapMaker().softValues().makeMap();
 
     private final Injector injector;
     
@@ -58,23 +50,6 @@ final class LegacyCommandCache implements CommandCache {
         final Class<?> type = forName(realName);
         return injector.getInstance(type);
     }
-    
-    private Class<?> forName(String name) {
-        final Class<?> cached = cache.get(name);
-        if (cached == null) {
-            try {
-                final Class<?> type = Class.forName(name);
-                LOG.trace("Putting {} into cache", type);
-                cache.put(name, type);
-                return type;
-            } catch (ClassNotFoundException e) {
-                throw new IllegalArgumentException(e);
-            }
-        } else {
-            LOG.trace("Retrieved {} from cache", cached);
-            return cached;
-        }
-    }
 
     private String toRealName(String aliasedName) {
         for (Alias alias : aliases) {
@@ -83,6 +58,14 @@ final class LegacyCommandCache implements CommandCache {
             }
         }
         return aliasedName;
+    }
+    
+    private Class<?> forName(String name) {
+        try {
+            return Classes.forName(name);
+        } catch (ClassNotFoundException e) {
+            throw new IllegalArgumentException(e);
+        }
     }
     
 }
