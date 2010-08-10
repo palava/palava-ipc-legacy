932129728d1389bd74e4f9b521008de474a6a71c
schoenborn <schoenborn@a41a61bc-0f84-421d-9ab3-fe1244b22163>
2010-08-10 15:00:03 +0000

    replaced String.format

--- src/main/java/de/cosmocode/palava/ipc/legacy/LegacyContentEncoder.java
+++ src/main/java/de/cosmocode/palava/ipc/legacy/LegacyContentEncoder.java
@@ -19,19 +19,23 @@ package de.cosmocode.palava.ipc.legacy;
 import javax.annotation.concurrent.ThreadSafe;
 
 import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBufferOutputStream;
 import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.channel.Channel;
-import org.jboss.netty.channel.ChannelHandler.Sharable;
 import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ChannelHandler.Sharable;
 import org.jboss.netty.handler.codec.oneone.OneToOneEncoder;
 
 import com.google.common.base.Charsets;
 
 import de.cosmocode.palava.bridge.Content;
-import de.cosmocode.palava.ipc.netty.ChannelBuffering;
 
 /**
- * An encoder which encodes {@link Content} to {@link ChannelBuffer}.
+ * An encoder which encodes {@link Content} to {@link ChannelBuffer} into the following form:
+ * 
+ * <p>
+ *   {@code mimeType://(contentLength)?content}
+ * </p>
  *
  * @since 1.0
  * @author Willi Schoenborn
@@ -40,23 +44,34 @@ import de.cosmocode.palava.ipc.netty.ChannelBuffering;
 @ThreadSafe
 final class LegacyContentEncoder extends OneToOneEncoder {
 
+    private static final byte[] COLON_SLASHES = "://(".getBytes(Charsets.UTF_8);
+    private static final byte[] QUESTION_MARK = ")?".getBytes(Charsets.UTF_8);
+    
     @Override
     protected Object encode(ChannelHandlerContext context, Channel channel, Object message) throws Exception {
         if (message instanceof Content) {
             final Content content = Content.class.cast(message);
-            final int length = (int) content.getLength();
-            final byte[] header = String.format("%s://(%s)?",
-                content.getMimeType(), Integer.toString(length)
-            ).getBytes(Charsets.UTF_8);
+            // cast to int is unsafe, but having more than 2GB is extremely unlikely
+            final int contentLength = (int) content.getLength();
             
-            final ChannelBuffer buffer = ChannelBuffers.dynamicBuffer(
-                header.length + length
+            final byte[] mimeType = content.getMimeType().toString().getBytes(Charsets.UTF_8);
+            final byte[] length = Integer.toString(contentLength).getBytes(Charsets.UTF_8);
+            
+            final ChannelBuffer buffer = ChannelBuffers.buffer(
+                mimeType.length + 
+                COLON_SLASHES.length + 
+                length.length + 
+                QUESTION_MARK.length + 
+                contentLength
             );
             
-            buffer.writeBytes(header);
+            buffer.writeBytes(mimeType);
+            buffer.writeBytes(COLON_SLASHES);
+            buffer.writeBytes(length);
+            buffer.writeBytes(QUESTION_MARK);
             
             // this effectively copies the content in memory
-            content.write(ChannelBuffering.asOutputStream(buffer));
+            content.write(new ChannelBufferOutputStream(buffer));
             
             return buffer;
         } else {
